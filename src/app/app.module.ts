9dfa355a0dc3f349011ed804a63c6a9be2670822
kanchan <kanchan.singh@walkingtree.tech>
2018-06-17 17:17:47 +0530

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, isDevMode } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { NgRedux, NgReduxModule, DevToolsExtension } from 'ng2-redux';
+
+import { AppComponent } from './app.component';
+import { IAppState, rootReducer, INITIAL_STATE } from './store';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoDashboardComponent } from './todo-dashboard/todo-dashboard.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    TodoDashboardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    NgReduxModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(ngRedux: NgRedux<IAppState>, devToolExtension: DevToolsExtension){
+    //ngRedux.configureStore(rootReducer, INITIAL_STATE);
+
+    //debugging
+    let enhancer = isDevMode() ? [devToolExtension.enhancer()]: [];
+    ngRedux.configureStore(rootReducer, INITIAL_STATE, [], enhancer);
+  }
+ }
